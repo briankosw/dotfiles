60c465ee6b32e6e2c4621a9a4ca3bda7f65cf3e8
Brian Ko <brian@loop.com>
2024-07-07 13:52:16 -0700

    Fix LSP settings

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -19,11 +19,12 @@ return {
       cmp.setup({
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'buffer' },
         },
-        mapping = {
+        mapping = cmp.mapping.preset.insert({
           -- Enter key confirms completion item
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        },
+        }),
         snippet = {
           expand = function(args)
             require('luasnip').lsp_expand(args.body)
